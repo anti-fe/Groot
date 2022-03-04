6a35a5865e1e94ab5dfb13203d810fce6053132c
anti <raylyangosha@gmail.com>
2022-03-04 12:13:50 +0300

    add smooth scroll

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const nav = document.querySelector('.header__nav');
+const mainLink = document.querySelector('.header__next'),
+    mainBlock = document.querySelector('.main');
 
+//Добавление подложки для навбара при скроле
 window.addEventListener('scroll', ()=>{
     nav.classList.toggle('nav_sticky', window.scrollY > 0);
+})
+
+//Плавный скролл до следующего блока при нажатии на ссылку 
+mainLink.addEventListener('click', (e)=>{
+    e.preventDefault();
+    mainBlock.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
 })
