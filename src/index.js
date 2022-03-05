3bb85fcdebc91d1103b6c475fe1d3eed156abb9b
anti <raylyangosha@gmail.com>
2022-03-05 20:00:34 +0300

    add auto-slider on main screen

--- src/index.js
+++ src/index.js
@@ -4,22 +4,23 @@ const nav = document.querySelector('.header__nav'),
 const mainLink = document.querySelector('.header__next'),
     mainBlock = document.querySelector('.main');
 
+
 //Добавление подложки для навбара при скроле
-window.addEventListener('scroll', ()=>{
+window.addEventListener('scroll', () => {
     nav.classList.toggle('nav_sticky', window.scrollY > 0);
 })
 
 //Плавный скролл до следующего блока при нажатии на ссылку 
-mainLink.addEventListener('click', (e)=>{
+mainLink.addEventListener('click', (e) => {
     e.preventDefault();
     mainBlock.scrollIntoView({
         behavior: 'smooth',
         block: 'start'
-      });
+    });
 })
 
 //Добавляем класс active к иконке бургера и ему самому
-burgerMenu.addEventListener('click', ()=>{
+burgerMenu.addEventListener('click', () => {
     burgerMenu.classList.toggle('active-burger');
-    modalBurger.classList.toggle('active-menu-burger');     
+    modalBurger.classList.toggle('active-menu-burger');
 })
---
+++ src/modules/sliders.js
@@ -0,0 +1,18 @@
+const header = document.querySelector('.header');
+const dataImgs = [
+    'src/images/first-screen1.png',
+    'src/images/first-screen2.png',
+    'src/images/first-screen3.png'
+];
+
+let countImg = 1;
+//Атоматический слайдер на главном экране
+function autoSliderBg() {
+    if(countImg > dataImgs.length-1) {
+        countImg = 0;
+    } else {
+        header.style.backgroundImage = `url('${dataImgs[countImg]}')`;
+        countImg++;
+    }
+}
+setInterval(autoSliderBg, 6000);
