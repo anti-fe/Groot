b4b850b0c5a2d9075bdd12f200104569f4d349b3
anti <raylyangosha@gmail.com>
2022-03-06 20:59:17 +0300

    add active-dote on auto-slider

--- src/modules/sliders.js
+++ src/modules/sliders.js
@@ -1,4 +1,6 @@
 const header = document.querySelector('.header');
+const sliderDots = Array.from(document.querySelectorAll('.header__dots-item'));
+console.log(sliderDots[1]);
 const dataImgs = [
     './src/images/first-screen1.png',
     './src/images/first-screen2.png',
@@ -8,10 +10,16 @@ const dataImgs = [
 let countImg = 1;
 //Атоматический слайдер на главном экране
 function autoSliderBg() {
+    sliderDots.forEach(dote=>{
+        dote.classList.remove('item-active');
+    })
     if(countImg > dataImgs.length-1) {
         countImg = 0;
+        header.style.backgroundImage = `url('${dataImgs[countImg]}')`;
+        sliderDots[countImg].classList.add('item-active');
     } else {
         header.style.backgroundImage = `url('${dataImgs[countImg]}')`;
+        sliderDots[countImg].classList.add('item-active');
         countImg++;
     }
 }
