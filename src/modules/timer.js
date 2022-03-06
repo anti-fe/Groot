0a754f307791f7c384bdd81b9583709e72364236
anti <raylyangosha@gmail.com>
2022-03-06 20:40:38 +0300

    add declinations for timer

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -7,7 +7,7 @@ const timerDays = document.querySelector('.new-collection__days'),
     minText = document.querySelector('.new-collection__min-text'),
     secText = document.querySelector('.new-collection__sec-text');
 
-const deadLine = new Date(2022, 2, 31, 16, 1, 0);
+const deadLine = new Date(2022, 2, 32, 59, 1, 0);
 
 let date,
     ourMls,
@@ -48,22 +48,86 @@ function setTimer() {
         arrDate.seconds = '00';
         clearInterval(intervalDate);
     }
-    //Проверка склонения
+    //Проверка склонения дней
     if (arrDate.days == 0) {
-        timerDays.innerHTML = '0 дней';
-    }
-    if (arrDate.days >= 11 && arrDate.days <= 20) {
+        daysText.innerHTML = 'дней';
+    } else if (arrDate.days >= 11 && arrDate.days <= 20) {
         daysText.innerHTML = `дней`;
     } else if (arrDate.days % 10 === 1) {
         daysText.innerHTML = `день`;
-    } else if ((arrDate.days >= 2 && arrDate.days <= 4) || (arrDate.days >= 22 && arrDate.days <= 24) || (arrDate.days >= 32 && arrDate.days <= 34)) {
+    } else if ((arrDate.days >= 2 && arrDate.days <= 4) || 
+                (arrDate.days >= 22 && arrDate.days <= 24) || 
+                (arrDate.days >= 32 && arrDate.days <= 34)) {
         daysText.innerHTML = `дня`;
-    } else if ((arrDate.days >= 5 && arrDate.days <= 9) || arrDate.days % 10 === 0) {
+    } else if ((arrDate.days >= 5 && arrDate.days <= 9) || (arrDate.days % 10 === 0) || (arrDate.days >= 25 && arrDate.days <= 29)) {
         daysText.innerHTML = `дней`;
     }
-    timerDays.innerHTML = `${arrDate.days}`;
-    timerHours.innerHTML = `${arrDate.hours}`;
-    timerMinutes.innerHTML = `${arrDate.minutes}`;
+
+    //Проверка склонения часов
+    if (arrDate.hours == 0) {
+        hoursText.innerHTML = 'часов';
+    } else if (arrDate.hours >= 11 && arrDate.hours <= 20) {
+        hoursText.innerHTML = `часов`;
+    } else if (arrDate.hours % 10 === 1) {
+        hoursText.innerHTML = `час`;
+    } else if ((arrDate.hours >= 2 && arrDate.hours <= 4) || 
+                (arrDate.hours >= 22 && arrDate.hours <= 24)) {
+        hoursText.innerHTML = `часа`;
+    } else if ((arrDate.hours >= 5 && arrDate.hours <= 9) || 
+                (arrDate.hours % 10 === 0)){
+        hoursText.innerHTML = `часов`;
+    }
+
+    //Проверка склонения минут
+    if (arrDate.minutes == 0) {
+        minText.innerHTML = 'минут';
+    } else if (arrDate.minutes >= 11 && arrDate.minutes <= 20) {
+        minText.innerHTML = `минут`;
+    } else if (arrDate.minutes % 10 === 1) {
+        minText.innerHTML = `минута`;
+    } else if ((arrDate.minutes >= 2 && arrDate.minutes <= 4) || 
+                (arrDate.minutes >= 22 && arrDate.minutes <= 24) || 
+                (arrDate.minutes >= 32 && arrDate.minutes <= 34) ||
+                (arrDate.minutes >= 42 && arrDate.minutes <= 44) ||
+                (arrDate.minutes >= 52 && arrDate.minutes <= 54)) {
+        minText.innerHTML = `минуты`;
+    } else if ((arrDate.minutes >= 5 && arrDate.minutes <= 9) || 
+                (arrDate.minutes % 10 === 0) ||
+                (arrDate.minutes >= 15 && arrDate.minutes <= 19) ||
+                (arrDate.minutes >= 25 && arrDate.minutes <= 29) ||
+                (arrDate.minutes >= 35 && arrDate.minutes <= 39) ||
+                (arrDate.minutes >= 45 && arrDate.minutes <= 49) ||
+                (arrDate.minutes >= 55 && arrDate.minutes <= 59)) {
+        minText.innerHTML = `минут`;
+    }
+
+    //Проверка склонения секунд
+    if (arrDate.seconds == 0) {
+        secText.innerHTML = 'секунд';
+    } else if (arrDate.seconds >= 11 && arrDate.seconds <= 20) {
+        secText.innerHTML = `секунд`;
+    } else if (arrDate.seconds % 10 === 1) {
+        secText.innerHTML = `секунда`;
+    } else if ((arrDate.seconds >= 2 && arrDate.seconds <= 4) || 
+                (arrDate.seconds >= 22 && arrDate.seconds <= 24) || 
+                (arrDate.seconds >= 32 && arrDate.seconds <= 34) || 
+                (arrDate.seconds >= 42 && arrDate.seconds <= 44) || 
+                (arrDate.seconds >= 52 && arrDate.seconds <= 54)) {
+        secText.innerHTML = `секунды`;
+    } else if ((arrDate.seconds >= 5 && arrDate.seconds <= 9)  ||
+                (arrDate.seconds >= 15 && arrDate.seconds <= 19) || 
+                (arrDate.seconds >= 25 && arrDate.seconds <= 29) || 
+                (arrDate.seconds >= 35 && arrDate.seconds <= 39) || 
+                (arrDate.seconds >= 45 && arrDate.seconds <= 49) || 
+                (arrDate.seconds >= 55 && arrDate.seconds <= 59) || 
+                (arrDate.seconds % 10 === 0)) {
+        secText.innerHTML = `секунд`;
+    }
+
+
+    timerDays.innerHTML = arrDate.days;
+    timerHours.innerHTML = arrDate.hours;
+    timerMinutes.innerHTML = arrDate.minutes;
     timerSeconds.innerHTML = arrDate.seconds;
 }
 
