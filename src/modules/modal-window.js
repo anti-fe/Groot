eb74fad3c3aecaff1300ab20e8f0306de6fc75ca
anti <raylyangosha@gmail.com>
2022-03-10 16:01:35 +0300

    add auth and reg in html/css

--- src/modules/modal-window.js
+++ src/modules/modal-window.js
@@ -1,11 +1,38 @@
 //Модальное окно
 const profileBtn = document.querySelectorAll('.profile-btn');
+const modalClose = document.querySelector('.modal-window__close');
+const toRegistr = document.querySelector('.modal-window__reg-btn');
+const toAuth = document.querySelector('.modal-window__auth-btn');
+
+const page = document.querySelector('body');
 const modalWindow = document.querySelector('.modal-window');
 const headerNav = document.querySelector('.header__nav');
+const modalAuth = document.querySelector('.modal-window__auth');
+const modalReg = document.querySelector('.modal-window__reg');
 
 profileBtn.forEach(btn=>{
     btn.addEventListener('click', ()=>{
         modalWindow.style.transform = 'translateY(1080px)';
         headerNav.style.display = 'none';
+        page.style.overflowY = 'hidden';
     })
+})
+
+modalClose.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modalWindow.style.transform = 'translateY(-1080px)';
+    headerNav.style.display = 'block';
+    page.style.overflowY = 'visible';
+})
+
+toRegistr.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modalAuth.style.display = "none";
+    modalReg.style.display = "grid";
+})
+
+toAuth.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modalAuth.style.display = "grid";
+    modalReg.style.display = "none";
 })
