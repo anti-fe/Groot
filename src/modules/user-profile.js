1594783208d0c6bf10a737ae6ef35389bd04240e
anti <raylyangosha@gmail.com>
2022-03-18 11:37:59 +0300

    add response footer

--- src/modules/user-profile.js
+++ src/modules/user-profile.js
@@ -5,6 +5,7 @@ const burgerMenu = document.querySelector('.nav__burger'),
 const mainWrapper = document.querySelector('.main__wrapper'),
         main = document.querySelector('.main'),
         mainCont = document.querySelectorAll('.main__cont');
+const footerSection = document.querySelectorAll('.footer__section');
 
 //Добавляем класс active к иконке бургера и ему самому
 burgerMenu.addEventListener('click', () => {
@@ -23,18 +24,12 @@ logOutBtn.forEach(item=>{
     })
 })
 
-gsap.registerPlugin(ScrollTrigger);
-gsap.to(mainCont, {
-    xPercent: -100 * (mainCont.length - 1),
-    ease: 'none',
-    duration: 6,
-    scrollTrigger: {
-        trigger: main,
-        pin: true,
-        scrub: true,
-        // snap: 1 / (mainCont.length - 1),
-        start: `-150px`,
-        end: `+=${main.offsetWidth}`
-    }
-})
+footerSection.forEach(item=>{
+    item.addEventListener('click', ()=>{
+        let itemList = item.querySelector('.footer__section-list');
+        let itemArrow = item.querySelector('.footer__section-head').querySelector('.footer__arrow');
 
+        itemList.classList.toggle('footer__active-list');
+        itemArrow.classList.toggle('footer__active-arrow');
+    })
+})
