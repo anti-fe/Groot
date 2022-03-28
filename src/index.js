e3e8c9a52f47137f189b4211bfea9ad9727c50f8
anti <raylyangosha@gmail.com>
2022-03-28 19:30:13 +0300

    add functional slider and footer on main page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ const nav = document.querySelector('.header__nav'),
 const mainLink = document.querySelector('.header__next'),
         mainBlock = document.querySelector('.main');
 const burgerProfileBtn = document.querySelector('.header__burger-profile'); 
+const footerSection = document.querySelectorAll('.footer__section');
+
+
+footerSection.forEach(item => {
+    item.addEventListener('click', () => {
+        let itemList = item.querySelector('.footer__section-list');
+        let itemArrow = item.querySelector('.footer__section-head').querySelector('.footer__arrow');
+        itemList.classList.toggle('footer__active-list');
+        itemArrow.classList.toggle('footer__active-arrow');
+    })
+})       
 
 //Добавление подложки для навбара при скроле
 window.addEventListener('scroll', () => {
--- src/modules/sliders.js
+++ src/modules/sliders.js
@@ -1,3 +1,7 @@
+const sliderContent = document.querySelector('.slider__content'),
+    sliderTrack = document.querySelector('.slider__track');
+let sliderMove = 0;
+let countSlides = 0;
 const header = document.querySelector('.header');
 const sliderDots = Array.from(document.querySelectorAll('.header__dots-item'));
 const dataImgs = [
@@ -5,6 +9,8 @@ const dataImgs = [
     './src/images/first-screen2.webp',
     './src/images/first-screen3.webp'
 ];
+
+const dataCollections = JSON.parse(localStorage.getItem('collections'));
 
 let countImg = 1;
 //Атоматический слайдер на главном экране
@@ -23,3 +29,77 @@ function autoSliderBg() {
     }
 }
 setInterval(autoSliderBg, 6000);
+
+
+//Слайдер "Другие товары"
+sliderContent.addEventListener('click',(e)=>{
+    const slides = document.querySelectorAll('.slider__card');
+    const parent = e.target.parentElement;
+    const secondParent = e.target.parentElement.parentElement;
+    const item = e.target;
+
+    if(item.classList.contains('slider__btn-prev') ||
+    parent.classList.contains('slider__btn-prev') || 
+    secondParent.classList.contains('slider__btn-prev')) {
+        if(countSlides <= 0) {
+            countSlides = slides.length-1;
+            sliderMove = 300
+            sliderTrack.style.transform = `translateX(-${sliderMove}%)`;
+        } else {
+            countSlides--;
+            sliderMove -= 100
+            sliderTrack.style.transform = `translateX(-${sliderMove}%)`;
+        }
+    } else if(item.classList.contains('slider__btn-next') ||
+    parent.classList.contains('slider__btn-next') || 
+    secondParent.classList.contains('slider__btn-next')) {
+        if(countSlides >= slides.length-1) {
+            countSlides = 0;
+            sliderMove = 0;
+            sliderTrack.style.transform = `translateX(${sliderMove}%)`;
+        } else {
+            countSlides++;
+            sliderMove += 100
+            sliderTrack.style.transform = `translateX(-${sliderMove}%)`;
+        }
+    }
+})
+
+sliderTrack.addEventListener('click',(e)=>{    
+    const parent = e.target.parentElement.parentElement.parentElement;
+    const parent2 = parent.parentElement;
+
+    const btnParent = e.target;
+    const btnParent2 = e.target.parentElement;
+
+    if(btnParent.classList.contains('slider__more-btn')) {
+        createProductPage(parent);
+    } else if (btnParent2.classList.contains('slider__more-btn')) {
+        createProductPage(parent2);
+    }
+})
+
+function createProductPage(item) {
+    console.log(item);
+    const collectionName = item.dataset['collectionname'];
+    const itemName = item.dataset['itemname'];
+    const itemType = item.dataset['itemtype'];
+    const itemPrice = item.dataset['itemprice'];
+    const itemMaterial = item.dataset['itemmaterial'];
+    const itemSize = item.dataset['itemsize'];
+    const itemPhoto = item.dataset['itemimg'];
+
+    const product = 
+    {
+        "collectionName": collectionName,
+        "itemName": itemName,
+        "itemType": itemType,
+        "itemPrice": itemPrice,
+        "itemMaterial": itemMaterial,
+        "itemSize": itemSize,
+        "itemPhoto": itemPhoto
+    };
+    console.log(product);
+    localStorage.setItem('product', JSON.stringify(product));
+    // window.location.href = 'src/pages/product.html';
+}
